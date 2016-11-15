c8dff7b04e859d3f05e2f2a6639bab7dc70960d6
Xiaoyang Liao <liao062@usc.edu>
2016-11-14 21:46:36 -0800

    leo123

--- Shelf_Motivation/app/src/main/java/com/a201csci/shelf_motivation/Goals.java
+++ Shelf_Motivation/app/src/main/java/com/a201csci/shelf_motivation/Goals.java
@@ -1,6 +1,6 @@
 package com.a201csci.shelf_motivation;
 //working
-//leo
+//leo123
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
