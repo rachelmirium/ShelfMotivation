aaeba2675b71ec485458bccbd153701c7eab65b0
Anish Verma <anishver@usc.edu>
2016-11-28 20:54:39 -0800

    error navigation

--- Shelf_Motivation/app/src/main/java/com/a201csci/shelf_motivation/BookInfo.java
+++ Shelf_Motivation/app/src/main/java/com/a201csci/shelf_motivation/BookInfo.java
@@ -161,6 +161,7 @@ public class BookInfo extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
+
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
@@ -168,15 +169,12 @@ public class BookInfo extends AppCompatActivity
         if (id == R.id.nav_search) {
             Intent intent = new Intent(this, Search.class);
             startActivity(intent);
-            //BooksAPI.getBookByID(this, "zyTCAlFPjgYC");
-        } else if (id == R.id.nav_bookshelf) {
+        }  else if (id == R.id.nav_bookshelf) {
             Intent intent = new Intent(this, BookshelfActivity.class);
             startActivity(intent);
-
         } else if (id == R.id.nav_bookclubs) {
-
             if ( ((Guest) this.getApplication()).getGuest()){
-                Intent intent = new Intent(this, GuestError.class);
+                Intent intent = new Intent(this, ErrorActivity.class);
                 startActivity(intent);
             }else {
                 Intent intent = new Intent(this, BookclubOverview.class);
@@ -184,7 +182,7 @@ public class BookInfo extends AppCompatActivity
             }
         } else if (id == R.id.nav_notifications) {
             if (((Guest) this.getApplication()).getGuest()) {
-                Intent intent = new Intent(this, GuestError.class);
+                Intent intent = new Intent(this, ErrorActivity.class);
                 startActivity(intent);
             } else{
                 Intent intent = new Intent(this, NotificationActivity.class);
@@ -193,12 +191,7 @@ public class BookInfo extends AppCompatActivity
         } else if (id == R.id.nav_goals) {
             Intent intent = new Intent(this, GoalActivity.class);
             startActivity(intent);
-
-        } else if (id == R.id.nav_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
